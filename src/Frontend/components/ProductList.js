e41689c3688890bb4975f99c91274df252096c47
Mahendra <mahendrathungha.99@gmail.com.com>
2025-06-04 11:22:41 +0530

    restore productlist and server.js

--- src/Frontend/components/ProductList.js
+++ src/Frontend/components/ProductList.js
@@ -5,7 +5,7 @@ const ProductList = () => {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    fetch('http://myapi.onrender.com/products')
+    fetch('http://localhost:8081/products')
       .then(res => res.json())
       .then(data => setProducts(data))
       .catch(err => console.error('Error fetching products:', err));
