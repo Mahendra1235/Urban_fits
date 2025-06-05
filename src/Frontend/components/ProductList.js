154e66b880ee6510eca52aa14c99affa86c730cb
Mahendra <mahendrathungha.99@gmail.com.com>
2025-06-05 16:07:11 +0530

    Commit ProductList.js changes

--- src/Frontend/components/ProductList.js
+++ src/Frontend/components/ProductList.js
@@ -4,8 +4,16 @@ import ProductCard from './ProductCard';
 const ProductList = () => {
   const [products, setProducts] = useState([]);
 
+  // useEffect(() => {
+  //   fetch('http://localhost:8081/products')
+  //     .then(res => res.json())
+  //     .then(data => setProducts(data))
+  //     .catch(err => console.error('Error fetching products:', err));
+  // }, []); 
   useEffect(() => {
-    fetch(`${process.env.REACT_APP_API_BASE_URL}/products`)
+    const apiBaseUrl = process.env.REACT_APP_API_BASE_URL;
+
+    fetch(`${apiBaseUrl}/products`)
       .then(res => res.json())
       .then(data => setProducts(data))
       .catch(err => console.error('Error fetching products:', err));
