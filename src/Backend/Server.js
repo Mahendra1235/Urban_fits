e1e3c593edb9aa744177800aa0a60d5f0b1d6922
Mahendra <mahendrathungha.99@gmail.com.com>
2025-06-05 11:57:53 +0530

    Commit changes

--- src/Backend/Server.js
+++ src/Backend/Server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(cors({
   // origin: ['http://your-vercel-app.vercel.app'],
   // origin: ['https://urban-fits.vercel.app'],
-  origin: ['http://localhost:3000/'],
+  origin: ['http://localhost:3000'],
   methods: ['GET', 'POST'],
   credentials:true
 }));
